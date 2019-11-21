bb16c96fa347db5609b4b914e20ececec431d250
Alla Sariieva <alla.cleo@gmail.com>
2019-11-21 12:58:15 +0200

    Updates

---
+++ precache-manifest.1fb792b7a9374619780dc9c84bd0b6cc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0c56c22341adf0a58c9ca9ea6f86c890",
+    "url": "/goit-react-hw-03-image-finder/index.html"
+  },
+  {
+    "revision": "1dff75d823621ddc5f20",
+    "url": "/goit-react-hw-03-image-finder/static/css/2.5dbdccff.chunk.css"
+  },
+  {
+    "revision": "6af2e139b5b9f1348f49",
+    "url": "/goit-react-hw-03-image-finder/static/css/main.27d8afaf.chunk.css"
+  },
+  {
+    "revision": "1dff75d823621ddc5f20",
+    "url": "/goit-react-hw-03-image-finder/static/js/2.3d59d7c0.chunk.js"
+  },
+  {
+    "revision": "6af2e139b5b9f1348f49",
+    "url": "/goit-react-hw-03-image-finder/static/js/main.90360164.chunk.js"
+  },
+  {
+    "revision": "3dc2d4bc992ea5be91b7",
+    "url": "/goit-react-hw-03-image-finder/static/js/runtime-main.7345331c.js"
+  }
+]);
